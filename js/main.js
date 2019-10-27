5c15c33e5398521d5682153794c020723888be09
Safe_Mode <safe2mode@gmail.com>
2019-10-27 15:04:44 +0300

    Adapt page header for 992

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
+const toggleMobileMenu = () => {
+  document.querySelector(`.js-nav-toggle`).classList.toggle(`nav-toggle--active`);
+  document.querySelector(`.js-logo`).classList.toggle(`logo--white`);
+  document.querySelector(`.js-user-area-sign`).classList.toggle(`user-area__sign--transparent`);
+  document.querySelector(`.js-mobile-menu`).classList.toggle(`page-header__mobile-menu--hidden`);
+};
+
+document.documentElement.classList.remove(`no-js`);
+toggleMobileMenu();
+
 document
   .querySelector(`.js-nav-toggle`)
   .addEventListener(`click`, (evt) => {
     evt.preventDefault();
-
-    evt.currentTarget.classList.toggle(`nav-toggle--active`);
-    document.querySelector(`.js-logo`).classList.toggle(`logo--white`);
-    document.querySelector(`.js-user-area-sign`).classList.toggle(`user-area__sign--transparent`);
-    document.querySelector(`.js-mobile-menu`).classList.toggle(`page-header__mobile-menu--hidden`);
+    toggleMobileMenu();
   });
